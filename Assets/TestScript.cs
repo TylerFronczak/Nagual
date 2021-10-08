d730cad90cc5b2ab3799e944db510914bf5c33ac
TylerFronczak <contact@tylerfronczak.com>
2021-10-08 02:43:05 -0400

    Testing version control

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -7,7 +7,7 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //asdassdsada
     }
 
     // Update is called once per frame
